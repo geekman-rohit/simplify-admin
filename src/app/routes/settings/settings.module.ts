c409ecf48504ca4899b896784774ac63544022e0
Rohit Mulange <rohitmulange@gmail.com>
2017-12-31 07:50:09 +0530

    carousel on website

---
+++ src/app/routes/settings/settings.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BillingComponent } from './billing/billing.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BillingComponent]
+})
+export class SettingsModule { }
