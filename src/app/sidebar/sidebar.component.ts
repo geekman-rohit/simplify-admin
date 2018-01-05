049d4c4445795f2ec12a204de06f244aa4b552ee
Rohit Mulange <rohitmulange@gmail.com>
2018-01-06 01:43:20 +0530

    fix bugs

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { menu } from '../menu';
 import { SettingsService } from '../helpers/settings.service';
+import { Router, ActivatedRoute, NavigationEnd, NavigationStart } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -10,10 +11,15 @@ export class SidebarComponent implements OnInit {
 
   menu = menu;
 
-  constructor(public settings: SettingsService) {
+  constructor(public settings: SettingsService, private router: Router) {
   }
 
   ngOnInit() {
+    this.router.events.subscribe((event: any) => {
+      if(event instanceof NavigationStart) {
+        this.settings.hideSidebar('left');
+      }
+    });
   }
   toggleSidebarVisibility() {
     this.settings.sidebar.left.visible = !this.settings.sidebar.left.visible;
